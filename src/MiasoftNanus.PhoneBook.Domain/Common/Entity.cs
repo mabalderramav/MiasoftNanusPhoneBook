f47298b107a446c6376e390c0c685f9053642e15
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2026-07-07 23:35:40 -0400

    MNPB-17 Refactor entity

--- src/MiasoftNanus.PhoneBook.Domain/Common/Entity.cs
+++ src/MiasoftNanus.PhoneBook.Domain/Common/Entity.cs
@@ -1,16 +1,6 @@
-﻿using MiasoftNanus.PhoneBook.Domain.Common.Interfaces;
-
-namespace MiasoftNanus.PhoneBook.Domain.Common;
+﻿namespace MiasoftNanus.PhoneBook.Domain.Common;
 
 public abstract class Entity(Guid id)
 {
     public Guid Id { get; init; } = id;
-    private readonly List<IDomainEvent> _domainEvents = [];
-
-    public List<IDomainEvent> PopDomainEvents()
-    {
-        var tmp = _domainEvents.ToList();
-        _domainEvents.Clear();
-        return tmp;
-    }
 }
